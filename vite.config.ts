f10465965c2ea57bc4ffa3e22fbd79b0c66fc575
xanpavle <sportsketeme@gmail.com>
2026-03-02 19:30:42 +0100

    Refactor Vite config to load environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-  base: "/Shadow-Verdict/"
-}) => {
+export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
